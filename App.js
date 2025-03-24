1337aa837005cb02e0a89129cc33ce0fb52d162c
Wubishet <wubishet05@gmail.com>
2025-03-24 15:51:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-
+// import from the onboarding
 import Onboarding from './src/screens/Onboarding';
 
 export default function App() {
